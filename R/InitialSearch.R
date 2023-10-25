8b1e501e96cb1ef09c561028616710759bb3e6e0
huiUofT <huisci@gmail.com>
2023-10-24 21:22:06 -0400

    20231024

--- R/InitialSearch.R
+++ R/InitialSearch.R
@@ -1,9 +1,9 @@
 #' This function is used for initial searching
 #'
-#' @param Library 
-#' @param ppm 
-#' @param polarity 
-#' @param Database 
+#' @param Library
+#' @param ppm
+#' @param polarity
+#' @param Database
 #'
 #' @return
 #' @export
@@ -14,14 +14,14 @@ InitialSearch<-function(Library,ppm,polarity,Database){
     Adducts<-c('[M+H]+','[M+H-H2O]+','[M+NH4]+','[M+CH3OH+H]+')
     MW.adducts<-c(1.007825,-17.00274,18.03437,33.03404)-0.00054
   }
-  
+
   if (polarity==-1){
     #Adducts<-c('[M]-','[M-H]-','[M-Br+O]-','[M-H-H2O]-','[M+Cl]-','[M+CH2O2-H]-')
     #MW.adducts<-c(0,-1.007825,-62.92342,-19.01839,34.96885,44.99765)+0.00054
     Adducts<-c('[M-H]-')
     MW.adducts<-c(-1.007825)+0.00054
   }
-  
+
   indexresults<-NULL
   Library$ID<-rep(0,nrow(Library))
   Library$SMILES<-rep(0,nrow(Library))
@@ -29,6 +29,7 @@ InitialSearch<-function(Library,ppm,polarity,Database){
   Library$LOGP<-rep(0,nrow(Library))
   Library$formula<-rep(0,nrow(Library))
   Library$mserror<-rep(0,nrow(Library))
+  Library$db<-rep(0,nrow(Library))
   for (i in 1:nrow(Library)){
     mz<-Library$mz[i]
     for (j in 1:length(MW.adducts)){
@@ -40,6 +41,11 @@ InitialSearch<-function(Library,ppm,polarity,Database){
           Library$ID[i]<-paste(index1,collapse = ';')
         }else{
           Library$ID[i]<-paste(c(Library$ID[i],index1),collapse = ';')
+        }
+        if (Library$db[i]==0){
+          Library$db[i]<-paste(c(Database$Database[index1]),collapse = ';')
+        }else{
+          Library$db[i]<-paste(c(Library$db[i],Database$Database[index1]),collapse = ';')
         }
         if (Library$SMILES[i]==0){
           Library$SMILES[i]<-paste(c(Database$SMILES[index1]),collapse = ';')
@@ -61,7 +67,7 @@ InitialSearch<-function(Library,ppm,polarity,Database){
           }
         for (k in 1:length(index1)){
             Library$LOGP[i]<-paste(c(Library$LOGP[i],Database$LOGP[index1[k]]),collapse = ';')
-          
+
         }
       }
     }##findout matching, ppm
